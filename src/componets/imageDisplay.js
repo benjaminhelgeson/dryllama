51132aff36cc8aada7ad7ef5f5816cd7d918b346
Benjamin Helgeson <helgeson@mac.lan>
2025-08-23 19:55:16 -0500

    Added CDN with Cloudfront

--- src/componets/imageDisplay.js
+++ src/componets/imageDisplay.js
@@ -1,23 +1,20 @@
-
 const ImageDisplay = ({
-	alt="",
-	src="",
-	className='inline-block h-48 p-1',
-	href="",
-	...rest
+  alt = "",
+  src = "",
+  className = "inline-block h-48 p-1",
+  href = "",
+  ...rest
 }) => {
-	let awsSrc = "https://dryllamaimages.s3.amazonaws.com/" + src;
-	let hrefSrc = "https://dryllamaimages.s3.amazonaws.com/" + href;
+  let awsSrc = "https://d1oycyo1rfoi7d.cloudfront.net/" + src;
+  let hrefSrc = "https://d1oycyo1rfoi7d.cloudfront.net/" + href;
 
-	return (
-		<>
-	      <a href={hrefSrc}>
-	        <img alt={alt} loading="lazy" src={awsSrc} className={className} />
-		  </a>
-		</>
-	);
-}
+  return (
+    <>
+      <a href={hrefSrc}>
+        <img alt={alt} loading="lazy" src={awsSrc} className={className} />
+      </a>
+    </>
+  );
+};
 
-export {
-	ImageDisplay
-}
+export { ImageDisplay };
--- src/componets/imageNoLinkDisplay.js
+++ src/componets/imageNoLinkDisplay.js
@@ -1,19 +1,16 @@
-
 const ImageNoLinkDisplay = ({
-	alt="",
-	src="",
-	className='inline-block p-1',
-	...rest
+  alt = "",
+  src = "",
+  className = "inline-block p-1",
+  ...rest
 }) => {
-	let awsSrc = "https://dryllamaimages.s3.amazonaws.com/" + src;
+  let awsSrc = "https://d1oycyo1rfoi7d.cloudfront.net/" + src;
 
-	return (
-		<>
-	        <img alt={alt} src={awsSrc} className={className} />
-		</>
-	);
-}
+  return (
+    <>
+      <img alt={alt} src={awsSrc} className={className} />
+    </>
+  );
+};
 
-export {
-	ImageNoLinkDisplay
-}
+export { ImageNoLinkDisplay };
